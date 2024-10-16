e837aa6afb11301220acd7ef4f100ba2db62d1c6
Alpha Zen <>
2024-10-16 11:32:25 +0400

    feat: preparing on the new system

--- rs/src/lib.rs
+++ rs/src/lib.rs
@@ -2,25 +2,25 @@ mod helpers;
 mod indicators;
 mod sdk;
 
-use crate::helpers::create_limit_orders::create_limit_orders;
+// use crate::helpers::create_limit_orders::create_limit_orders;
 // use crate::helpers::create_signals::create_signals;
-use crate::helpers::round_value::round_value;
-use ndarray::Ix1;
+// use crate::helpers::round_value::round_value;
+// use ndarray::Ix1;
 // use indicators::cross_above::cross_above;
 // use indicators::cross_below::cross_below;
 use pyo3::prelude::*;
-use sdk::backtest::Backtest;
-use sdk::backtest_params::BacktestParams;
-use sdk::bbo::BBO;
-use sdk::contract::Contract;
-use sdk::enums::{OrderType, Side};
-use sdk::instrument::Instrument;
-// use sdk::ohlc::OHLCs;
-use sdk::order::LimitOrders;
-use sdk::position::Position;
+// use sdk::backtest::Backtest;
+// use sdk::backtest_params::BacktestParams;
+// use sdk::bbo::BBO;
+// use sdk::contract::Contract;
+// use sdk::enums::{OrderType, Side};
+// use sdk::instrument::Instrument;
+use sdk::ohlc::OHLCs;
+// use sdk::order::LimitOrders;
+// use sdk::position::Position;
 use sdk::signal::Signal;
-use sdk::signals::Signals;
-use sdk::strategy::{Bar, Foo, Strategy, BT};
+// use sdk::signals::Signals;
+// use sdk::strategy::{Bar, Foo, Strategy, BT};
 
 #[pymodule]
 fn rs(m: &Bound<'_, PyModule>) -> PyResult<()> {
