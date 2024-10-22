f627a17c9d10df8cfb287aa698d81e3e7e796036
Alpha Zen <>
2024-10-22 10:19:09 +0400

    feat: v2 is starting to get ready

--- rs/src/sdk/ohlc.rs
+++ rs/src/sdk/ohlc.rs
@@ -8,52 +8,16 @@ use std::ops::Div;
 
 use serde::Serialize;
 
+// A representation of a time based Bar
 #[pyclass]
 #[derive(Debug, Clone, Serialize, Copy)]
-pub struct OHLC {
+pub struct OHLCV {
     pub date: DateTime<Utc>,
     pub open: Decimal,
     pub high: Decimal,
     pub low: Decimal,
     pub close: Decimal,
     pub volume: Decimal,
-}
-
-impl ToPyObject for OHLC {
-    fn to_object(&self, py: Python) -> PyObject {
-        let dict = PyDict::new_bound(py);
-        dict.set_item("date", self.date.to_rfc3339()).unwrap();
-        dict.set_item("open", self.open).unwrap();
-        dict.set_item("high", self.high).unwrap();
-        dict.set_item("low", self.low).unwrap();
-        dict.set_item("close", self.close).unwrap();
-        dict.set_item("volume", self.volume).unwrap();
-        dict.to_object(py)
-    }
-}
-
-#[pymethods]
-impl OHLC {
-    #[new]
-    fn new(
-        timestamp: Decimal,
-        open: Decimal,
-        high: Decimal,
-        low: Decimal,
-        close: Decimal,
-        volume: Decimal,
-    ) -> Self {
-        let date =
-            DateTime::from_timestamp(timestamp.to_i64().unwrap(), 0).expect("Invalid timestamp");
-        OHLC {
-            date,
-            open,
-            high,
-            low,
-            close,
-            volume,
-        }
-    }
 }
 
 // Where we create the Actual OHLCs
