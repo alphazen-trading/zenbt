27af9f47be56584348c4ec8a86242124b2be5f4c
Alpha Zen <>
2024-10-31 17:34:59 +0400

    feat: initial code for limit orders

--- rs/src/strategy/base.rs
+++ rs/src/strategy/base.rs
@@ -87,126 +87,31 @@ impl Strategy {
         10
     }
 
-    // #[classmethod]
-    // fn override_test(_cls: &Bound<'_, PyType>) -> PyResult<i32> {
-    //     println!("print within rust");
-    //     Ok(10)
-    // }
+    #[pyo3(signature = (index, client_order_id, side, size, price, sl=None, tp=None))]
+    #[allow(clippy::similar_names, clippy::too_many_arguments)]
+    pub fn create_limit_order(
+        &self,
+        index: usize,
+        client_order_id: String,
+        side: Side,
+        size: Decimal,
+        price: Decimal,
+        sl: Option<Decimal>,
+        tp: Option<Decimal>,
+    ) -> Order {
+        let _ = self;
+        Order {
+            index,
+            client_order_id,
+            order_type: OrderType::Limit,
+            side,
+            size,
+            price: Some(price),
+            sl,
+            tp,
+        }
+    }
 
-    // fn backtest(slf: &Bound<Self>) -> PyResult<i32> {
-    //     println!("print from rust: {}", 23);
-    //     let action: Action = slf
-    //         .call_method("override_test", (), None)
-    //         .unwrap()
-    //         .extract()
-    //         .unwrap();
-    //     println!("The cls is: {:?}", action);
-    //     println!("The cls is: {:?}", action.desired_orders);
-    //     // let action: Action = slf.getattr("desired_action").unwrap().extract().unwrap();
-    //     // println!("The value of it is: {:?}", action);
-    //     // println!("The value of it is: {:?}", action.desired_orders);
-    //     Ok(32)
-    // }
-    // fn backtest(&mut self, py: Python<'_>, wraps: Py<PyAny>) {
-    //     let df = self.df.0.clone();
-    //     // let res = slf.call_method_bound(py, intern!(py, "_on_candle"), (0, self), None);
-    //     let res: Action = wraps
-    //         .call_bound(py, (0, self.state.clone()), None)
-    //         .unwrap()
-    //         .extract(py)
-    //         .unwrap();
-    //     println!("The result is: {:?}", res);
-    //     for i in 0..df.height() {
-    //         //     // for (j, position) in &mut backtest.positions.active_positions.iter_mut().enumerate() {
-    //         //     // for pos in &mut self.active_positions {
-    //         //     //     println!("{:?}", pos);
-    //         //     // }
-    //         //
-    //         // let res = slf.call_method_bound(py, intern!(py, "_on_candle"), (i,), None);
-    //         // let action = res;
-
-    //         // // let action: Action = slf
-    //         // //     .call_method_bound(py, intern!(py, "_on_candle"), (i,), None)
-    //         // //     .unwrap()
-    //         // //     .extract(py)
-    //         // //     .unwrap();
-    //         // println!("The result is: {:?}", action);
-    //     }
-    // }
-    // fn foo(slf: &Bound<Self>) -> PyResult<()> {
-    //     slf.call_method0("override_test")?;
-    //     Ok(())
-    // }
-    // fn backtest(&self, py: Python<'_>) {
-    //     Self::override_test(self.into_py(py));
-    // }
-
-    // fn backtest(&self, py: Python<'_>) {
-    //     let cls = py.get_type_bound::<Strategy>();
-    //     let df = cls.getattr("desired_action");
-    //     let result = cls
-    //         .call_method(intern!(py, "override_test"), (), None)
-    //         .unwrap();
-    //     //     // println!("The cls is: {:?}", cls);
-    //     println!("The cls is: {:?}", df);
-    // }
-
-    //     let result = cls
-    //         .call_method(intern!(py, "override_test"), (), None)
-    //         .unwrap();
-    //     println!("The cls is: {:?}", result);
-    //     // .unwrap()
-    //     // .extract(py)
-    //     // .unwrap();
-    //     // cls.call_method(py, "override_test", (self), None).unwrap();
-    //     // self.into_py(py)
-    //     //     .call_method_bound(py, "_on_candle", (), None)
-    //     //     .unwrap();
-    //     // let ret = self.wraps.call_bound(py, (self.into_py(py)), None).unwrap();
-    //     // println!("{:?}", ret);
-    //     // self.wraps.call_method_bound(
-    //     //     py,
-    //     //     // intern!(py, "_on_candle"),
-    //     //     "_on_candle",
-    //     //     (0, self),
-    //     //     None,
-    //     // )
-    //     // let cls = &Self::override_test(self.borrow
-
-    //     // py.allow_threads(move || {
-    //     //     // An example of an "expensive" Rust calculation
-    //     //     // let sum = numbers.iter().sum();
-
-    //     //     Ok(1)
-    //     // })
-    //     // Python::with_gil(|py| {
-    //     //     Self::
-    //     //     // let res = Self::test(&Self::type_object_bound(py));
-    //     //     // println!("The res is: {}", res);
-    //     //     // Self::call_method_bound(py, intern!(py, "test"), (), None).unwrap();
-
-    //     //     let result: Action = Self::test(&Self::type_object_bound(py));
-    //     //     println!("The result is: {:?}", result);
-    //     //     // .strategy
-    //     //     // .call_method_bound(
-    //     //     //     py,
-    //     //     //     intern!(py, "_on_candle"),
-    //     //     //     (i, self.state.borrow(py), test.clone()),
-    //     //     //     None,
-    //     //     // )
-    //     //     // .unwrap()
-    //     //     // .extract(py)
-    //     //     // .unwrap();
-    //     //     // result.extract::<Action>(py).unwrap()
-    //     // })
-    // }
-
-    // #[classmethod]
-    // fn backtest(cls: &Bound<'_, PyType>) {
-    //     let result = cls.call_method("override_test", (cls,), None).unwrap();
-    //     let action = cls.getattr("desired_action").unwrap();
-    //     println!("The result is: {:?}", action);
-    // }
     #[pyo3(signature = (index, client_order_id, side, size, sl=None, tp=None))]
     #[allow(clippy::similar_names)]
     pub fn create_market_order(
