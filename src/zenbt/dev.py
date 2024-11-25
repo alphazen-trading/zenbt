2f0a220df9f530934b9f8f4215d96bb35c3ebfa8
Alpha Zen <>
2024-11-25 17:54:13 +0100

    feat: putting current

--- src/zenbt/dev.py
+++ src/zenbt/dev.py
@@ -1,25 +1,16 @@
-from data.data import read_data, read_data_pl, download_okx_data
 import time
 import talib
-from rich import print
 import polars as pl
-from sdk.base import BaseStrategy
 from zenbt.rs import (
-    OrderType,
-    Side,
-    Strategy,
-    BacktestParams,
-    PySharedState,
     Backtest,
-    Action,
-    Order,
-    Position,
     cross_above,
     cross_below,
+    BacktestParams,
+    Backtest,
 )
+from zenbt.ma_cross import MaCross
+from data.data import read_data_pl
 
-from sdk.stats import Stats
-from typing import Optional
 
 COMMISSION = 0
 COMMISSION = 0.02 / 100
@@ -32,43 +23,11 @@ bt_params = BacktestParams(
 )
 
 
-class ST(BaseStrategy):
-    def on_candle(self, state=None, **kwargs) -> Action:  # type: ignore
-        cross_below = self.data["cross_below"][self.index]
-        cross_above = self.data["cross_above"][self.index]
-
-        # Check for bullish cross over
-        if cross_above:
-            order = self.create_market_order(
-                self.index,
-                client_order_id="Long",
-                side=Side.Long,
-                size=self.default_size,
-            )
-            self.action.orders = {order.client_order_id: order}
-            self.action.close_all_positions = True
-
-        # Check for bearish crossover
-        if cross_below:
-            order = self.create_market_order(
-                self.index,
-                client_order_id="Short",
-                side=Side.Short,
-                size=self.default_size,
-            )
-            self.action.orders = {order.client_order_id: order}
-            self.action.close_all_positions = True
-
-        return self.action
-
-
 def dev():
     sym = "1000PEPE"
     df = read_data_pl(sym, 0, -1, resample_tf="1min", exchange="binance")
     # sym = "BTC"
     # df = read_data_pl(sym, 0, 200, resample_tf="1min", exchange="okx")
-
-    # backtest_old(df)
 
     fast_ma = talib.SMA(df["close"], timeperiod=10)
     slow_ma = talib.SMA(df["close"], timeperiod=50)
@@ -77,8 +36,8 @@ def dev():
         pl.Series("cross_above", cross_above(fast_ma, slow_ma)),
         pl.Series("cross_below", cross_below(fast_ma, slow_ma)),
     )
-    st = ST(df, default_size=1)
-    bt = Backtest(df, bt_params, st)
+    ma_cross = MaCross(df, default_size=1)
+    bt = Backtest(df, bt_params, ma_cross)
 
     start = time.time()
     bt.backtest()
